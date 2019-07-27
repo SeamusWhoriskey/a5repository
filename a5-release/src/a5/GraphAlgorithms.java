4608139c0de6249547f230e6b358146237ec7c12
Seamus Whoriskey <smw364@cornell.edu>
2019-07-27 15:48:22 -0400

    Wonky method, gonna fix now

--- a5-release/src/a5/GraphAlgorithms.java
+++ a5-release/src/a5/GraphAlgorithms.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.Stack;
 
@@ -64,8 +65,8 @@ public class GraphAlgorithms  {
 		Set<N> visited = new HashSet<N>();
 		visited.add(start);
 		// distances stores the distance from the start Node to Node N as a double.
-		Heap<N, Integer> 	distances 	= new Heap<N, Integer>(Comparator.reverseOrder());
-		distances.add(start, 0);
+		HashMap<N, Integer> 	distances 	= new HashMap<N, Integer>();
+		distances.put(start, 0);
 		// parents stores the parent of a Node n as a Node.
 		HashMap<N, N> 		parents 	= new HashMap<N, N>();
 		// all_visited is a boolean determining if all nodes in the graph have been visited
@@ -74,13 +75,34 @@ public class GraphAlgorithms  {
 		N node_curr = start;
 		
 		while (!all_visited) {
-			
-//			Node node_min = neighbors_dist.poll();
+			Heap<N, Integer> neighbors_dist = getWeightHeap(node_curr, visited);
+			try {
+				N node_min = neighbors_dist.poll();
+				visited.add(node_min);
+				
+				for (N vis_neighbor : node_min.outgoing().keySet()) {
+//					if (!visited.contains(vis_neighbor)) {
+						if (!distances.keySet().contains(node_min)) {
+							System.out.println("we're getting here");
+						}
+						int new_dist = distances.get(node_min) + 
+								getEdgeWeight(node_min, vis_neighbor);
+						if (new_dist < distances.get(vis_neighbor)) {
+							distances.put(vis_neighbor, new_dist);
+							parents.put(vis_neighbor, node_min);
+							
+						}
+						}
+//					}
+				
+			} catch (NoSuchElementException e)  {
+				break;
+			}
 		}
 		
 
 		 
-		
+		System.out.println(parents.toString());
 		return null;
 	}
 	
@@ -91,15 +113,17 @@ public class GraphAlgorithms  {
 		return node_from.outgoing().get(node_to).label();
 	}
 	
-	/** Returns a heap of nodes with priority of edge weight
-	 *  of the edge between node_from and its neighbors. */
+	/** Returns a heap of nodes with priority of edge weight of the edge 
+	 * between node_from and its neighbors that are not in the Set vis. */
 	private static <N extends Node<N,E>, E extends LabeledEdge<N,E,Integer>>
-	Heap<N, Integer> getMinEdge(N node_from) {
-		Heap<N, Integer> neighbors_dist 	= new Heap<N, Integer>(Comparator.reverseOrder());
+	Heap<N, Integer> getWeightHeap(N node_from, Set<N> vis) {
+		Heap<N, Integer> n_dist 	= new Heap<N, Integer>(Comparator.reverseOrder());
 		for (N neighbor : node_from.outgoing().keySet()) {
-			neighbors_dist.add(neighbor, getEdgeWeight(node_from, neighbor));
+			if (!vis.contains(neighbor)) {
+				n_dist.add(neighbor, getEdgeWeight(node_from, neighbor));
+			}
 		}
-		return null;
+		return n_dist;
 	}
 	
 }
