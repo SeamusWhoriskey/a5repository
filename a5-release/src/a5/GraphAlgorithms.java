61e590fa3af6e28a215ec2750a313c03f46eb2f7
Seamus Whoriskey <smw364@cornell.edu>
2019-07-29 13:43:41 -0400

    Small error in heapifyDown and heapifyUp methods corrected. (also minor change to poll method for readability)

--- a5-release/src/a5/GraphAlgorithms.java
+++ a5-release/src/a5/GraphAlgorithms.java
@@ -57,50 +57,59 @@ public class GraphAlgorithms  {
 	 */
 	public static <N extends Node<N,E>, E extends LabeledEdge<N,E,Integer>>
 	List<N> shortestPath(N start, N end) {
-//		System.out.println(start.toString() + " " + end.toString());
-		if (start.equals(end)) {
-			List<N> out_list = new ArrayList<N>();
-			out_list.add(start);
-			return out_list;
-		}
-		// To check the neighbors of a node n use:
-		// n.outgoing().keySet()
 		
-		// Set containing all the visited nodes.
-		// To check if a node n is unvisited use:
-		// !visited.contains(n)
+		// Set unvisited contains all the unvisited nodes.
 		Set<N> unvisited = new HashSet<N>(dfs(start));
-		// distances stores the distance from the start Node to Node N as a double.
-		HashMap<N, Double> 	distances 	= new HashMap<N, Double>();
+		
+		// If there is no connection from start to end, return an empty List.
+		if (!unvisited.contains(end)) {
+			return new ArrayList<N>();
+		}
+		
+		// HashMap distances stores the distance 
+		// from Node start to Node N as a Double.
+//		HashMap<N, Double> 	distances 	= new HashMap<N, Double>();
 		
 		// parents stores the parent of a Node n as a Node.
 		HashMap<N, N> 		parents 	= new HashMap<N, N>();
+		// Initializations of values for distances and parents.
+		Heap<N, Double> neighbor_dist = new Heap<N, Double>(Comparator.reverseOrder());
 		for (N neighbor : unvisited) {
-			distances.put(neighbor, Double.POSITIVE_INFINITY);	
+			neighbor_dist.add(neighbor, Double.POSITIVE_INFINITY);
+			// set all distances to positive infinity
+//			distances.put(neighbor, Double.POSITIVE_INFINITY);	
+			// set the parent of each neighbor to null
 			parents.put(neighbor, null);	
 		}
-		distances.put(start, 0.0);
-		// node_curr is the current node of Dijkstra's algorithm.
-		N node_curr;
-		// neighbor_dist is a Heap that will contain the distance from curr_node to each neighbor 
-		Heap<N, Double> neighbor_dist = new Heap<N, Double>(Comparator.reverseOrder());
-		// path complete ADSLFHADHJFBLA
-		boolean path_complete = true;
+		// set the distance of the start node to itself as 0.
+//		distances.put(start, 0.0);
+		neighbor_dist.changePriority(start, 0.0);
 		
+		
+//		Heap<N, Double> neighbor_dist = getWeightHeap(distances, unvisited);
+		
+		// While there are still unvisited nodes, 
 		while (!unvisited.isEmpty()) {
+			// neighbor_dist is a Heap that contains the distance 
+			// from curr_node to each neighbor of curr_node.
+//			Heap<N, Double> neighbor_dist = getWeightHeap(distances, unvisited);			
 			
+			// node_curr denotes the current node of Dijkstra's algorithm.
+			N node_curr = neighbor_dist.peek();
 			
-			neighbor_dist = getWeightHeap(distances, unvisited);
-			// If there are no unvisited nodes that are a neighbor of the current node,
-			if (neighbor_dist.size() == 0) {
-				path_complete = false;
-				break; 	
+			Double dist_curr = neighbor_dist.getPriority(node_curr);
+			neighbor_dist.poll();
+			
+			if (!neighbor_dist.checkHeap(0)) {
+				neighbor_dist.heapifyDown(0);
+				System.out.println(neighbor_dist.toString());
+				
 			}
-			node_curr = neighbor_dist.poll();
-
-			// If the current node's closest neighbor is not connected, pass
-			if (distances.get(node_curr) == Double.POSITIVE_INFINITY) {
-				path_complete = false;
+			
+			
+			// If the current node's closest neighbor is not connected, break
+			if (dist_curr == Double.POSITIVE_INFINITY) {
+				unvisited.remove(node_curr);
 				break; 	
 			}
 			
@@ -109,25 +118,21 @@ public class GraphAlgorithms  {
 			
 			for (N neighbor : node_curr.outgoing().keySet()) {
 				if (unvisited.contains(neighbor)) {
-					double new_weight = distances.get(node_curr) + getEdgeWeight(node_curr, neighbor);
-					if (!distances.containsKey(neighbor) || new_weight < distances.get(neighbor))  {
-						distances.put(neighbor, new_weight);
+					double new_weight = dist_curr
+										+ getEdgeWeight(node_curr, neighbor);
+					if (new_weight < neighbor_dist.getPriority(neighbor))  {
+						neighbor_dist.changePriority(neighbor, new_weight);
+//						distances.put(neighbor, new_weight);
 						parents.put(neighbor, node_curr);
 					}
 				}
 			}
-//			System.out.println("unvisited list: "+ unvisited.toString());
 
 			
 		}
-		
-		if (path_complete) {
-			List<N> n = parentsToList(parents, start, end);
-//			System.out.println("Out list: " + n.toString());
-			return n;
-		}
-//		System.out.println(path_complete);
-		return new ArrayList<N>();
+
+		return parentsToList(parents, start, end);
+
 	}
 	
 	
@@ -137,13 +142,20 @@ public class GraphAlgorithms  {
 		return node_from.outgoing().get(node_to).label();
 	}
 	
-	/** Returns a heap of nodes with priority of edge weight of the edge 
-	 * between node_from and its neighbors that are not in the Set vis. */
+	/** Returns a Heap of nodes with priority of edge weight of the edge 
+	 * between node_from and its neighbors that are in the Set unvis.
+	 * 
+	 *  @param dist
+	 *  @param unvis */
 	private static <N extends Node<N,E>, E extends LabeledEdge<N,E,Integer>>
 	Heap<N, Double> getWeightHeap(HashMap<N, Double> dist, Set<N> unvis) {
+		// n_dist is the Heap to be returned. Will contain unvisited Nodes sorted
+		// in order of least distance to greatest distance from the start node.
 		Heap<N, Double> n_dist 	= new Heap<N, Double>(Comparator.reverseOrder());
-		for (N neighbor : unvis) {
-			n_dist.add(neighbor, dist.get(neighbor));
+		// For each unvisited node,
+		for (N node : unvis) {
+			// Add node to n_dist with priority given in dist.
+			n_dist.add(node, dist.get(node));
 		}
 		return n_dist;
 	}
@@ -155,13 +167,9 @@ public class GraphAlgorithms  {
 		N last_added = end;
 		while (!out_list.contains(start)) {
 			last_added = parents.get(last_added);
-			if (last_added == null) {
-				return new ArrayList<N>();
-			}
 			out_list.add(last_added);
-			
-
-//			System.out.println(out_list.toString());
+//			System.out.println(start.toString() + " " + end.toString() + " " + out_list.toString());
+//			break;
 		}
 		Collections.reverse(out_list);
 		return out_list;
